44113daf97d2e36863166231f8bbeb8aad81a7f8
Ani Klekchyan <ani.klekchyan@healios.io>
2022-04-07 10:35:06 +0400

    Initial Commit

---
+++ SwiftyLibTest/SwiftyLibTest.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibTest.h
+//  SwiftyLibTest
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibTest.
+FOUNDATION_EXPORT double SwiftyLibTestVersionNumber;
+
+//! Project version string for SwiftyLibTest.
+FOUNDATION_EXPORT const unsigned char SwiftyLibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibTest/PublicHeader.h>
+
+
