8299a8edf5d5e3502688d3c98c1e019499d53164
Ani Klekchyan <ani.klekchyan@healios.io>
2022-04-07 10:38:40 +0400

    Initial project setup

---
+++ SwiftyLibTestExampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyLibTestExampllApp
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftyLibTestExampleApp/SwiftyLibTestExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibTestExampllAppApp.swift
+//  SwiftyLibTestExampllApp
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibTestExampllAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
