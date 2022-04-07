fb813015726e04695f2a34511d8c1ec41866fa4d
Ani Klekchyan <ani.klekchyan@healios.io>
2022-04-07 11:50:52 +0400

    little functionality added

---
+++ SwiftyLibTest/SwiftyLibTest.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLibTest.swift
+//  SwiftyLibTest
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
